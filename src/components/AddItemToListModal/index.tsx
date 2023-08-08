9701dff04b2ae9ed4dbb81261449219cc696e36d
Richard Melo <rmelocont@gmail.com>
2023-08-08 15:19:35 -0300

    UPDATE scroll into new item

--- src/components/AddItemToListModal/index.tsx
+++ src/components/AddItemToListModal/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import useList from "../../hooks/useList";
 import { Button } from "../ui/button";
 import { Input } from "../ui/input";
@@ -10,6 +10,7 @@ export default function AddItemToListModal({
 }) {
   const { addToGeneralList } = useList();
   const [name, setName] = useState("");
+  const newItemRef = useRef<HTMLInputElement | null>(null);
 
   const onNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setName(e.target.value);
@@ -30,14 +31,18 @@ export default function AddItemToListModal({
   };
 
   return (
-    <div className="text-sm mb-16 h-[40vh] py-4">
+    <div className="text-sm mb-16 h-[50vh] py-4">
       <div className="mb-2">
         <Input
+          ref={newItemRef}
           placeholder="Insira o nome do item"
           type="text"
           value={name}
           onChange={onNameChange}
           autoFocus
+          onFocus={() => {
+            newItemRef?.current?.scrollIntoView();
+          }}
           onKeyDown={(e) => {
             if (e.key == "Enter") insertNewItem();
           }}
